5c196463af57d244517d0a3771109c98b23b6c7f
michaeldimmitt <michaelgdimmitt@gmail.com>
2019-07-17 00:11:50 -0400

    update helper

--- app/helpers/income-expense.js
+++ app/helpers/income-expense.js
@@ -1,8 +1,6 @@
-import { helper as buildHelper } from '@ember/component/helper';
+import { helper } from '@ember/component/helper';
 
-export function incomeExpense(number/*, hash*/) {
+export default helper(function incomeExpense(number/*, hash*/) {
   if (number === 0) { return null; }
   return number > 0 ? 'income' : 'expense';
-}
-
-export default buildHelper(incomeExpense);
+})
